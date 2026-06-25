882fff426fac28b1eb3927d92b18f185dfa9bbcd
Alex Abbatiello <50689733+synthic@users.noreply.github.com>
2026-06-25 03:44:56 -0500

    Fix message handling

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const msgData = require('./messages.json')
 
 const app = express()
 
 const hostname = process.env.HOSTNAME || 'localhost'
 const ip = process.env.SERVER_IP || 2130706433
 const blazePort = process.env.PORT || 42127
-
-const msgData = {
-  messages: [
-    {
-      destination: '16679',
-      text: 'Welcome, Runner! Directly from the DICE developers, here are 3 great tips that will help you out in the city of Glass:\n1) Attacks from above, ziplines, or swingbars always deal much higher damage to enemies.\n2) The Disruptor is great for taking down those pesky security cameras.\n3) The unlockable movement skills will greatly help when trying to beat timed missions.\n\nFor more tips see mirrorsedge.com',
-      url: 'http://media.mirrorsedge.com/background.png',
-      'tracking-tag': '1337',
-      data: [
-        {
-          Key: 'TitleCaption',
-          Value: 'Welcome, Runner'
-        },
-        {
-          Key: 'Subcaption',
-          Value: 'We look forward to seeing you in the city of Glass'
-        },
-        {
-          Key: 'ActionLabel',
-          Value: 'Top 3 Tips & Tricks'
-        },
-        {
-          Key: 'PopupActionLabel',
-          Value: ''
-        },
-        {
-          Key: 'ActionPath',
-          Value: ''
-        }
-      ]
-    }
-  ]
-}
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>\
 <serverinstanceinfo>\
@@ -55,15 +23,25 @@ const xml = `<?xml version="1.0" encoding="UTF-8"?>\
 </serverinstanceinfo>`
 
 app.use(morgan('short'))
+app.use(express.json())
 app.use(express.static('public'))
 
-app.get('/redirector/getServerInstance', (req, res) => {
+app.post('/redirector/getServerInstance', (req, res) => {
   res.type('xml')
   res.send(xml)
 })
 
-app.get('/em/v2/messages', (req, res) => {
-  res.json(msgData)
+app.post('/em/v2/messages', (req, res) => {
+  const destination = req.body['destination-list'][0]
+  let data = {}
+
+  msgData['messages'].forEach(message => {
+    if (message['destination'] === destination) {
+      data = { messages: [ message ] }
+    }
+  })
+
+  res.json(data)
 })
 
 app.listen(42230, () => {
